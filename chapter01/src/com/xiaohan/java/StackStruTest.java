551fce5264a553895ccd10cd5137a87c3b89c599
hankang370 <hankang370@bupt.edu.cn>
2020-11-16 23:23:51 +0800

    第一章+JVM架构

---
+++ chapter01/src/com/xiaohan/java/StackStruTest.java
@@ -0,0 +1,15 @@
+package com.xiaohan.java;
+
+/**
+ * @Description:
+ * @Author: 小韩同学
+ * @Date: 2020/11/16
+ */
+public class StackStruTest {
+    public static void main(String[] args) {
+        // int i = 2 + 3;
+        int i = 2;
+        int j = 3;
+        int k = i + j;
+    }
+}
